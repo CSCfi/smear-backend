a9f9763cb4638452f4e09327b442a19b222cece0
Mikko Kautto <mikko.kautto@codento.com>
2020-04-24 15:26:19 +0300

    Table metadata endpoint stub

--- src/main/java/fi/csc/avaa/smear/constants/Endpoints.java
+++ src/main/java/fi/csc/avaa/smear/constants/Endpoints.java
@@ -5,4 +5,5 @@ public class Endpoints {
     private static final String API = "/api";
     public static final String STATIONS = API + "/stations";
     public static final String TIMESERIES = API + "/timeseries";
+    public static final String TABLE_METADATA = API + "/tablemetadata";
 }
---
+++ src/main/java/fi/csc/avaa/smear/resource/TableMetadataResource.java
@@ -0,0 +1,9 @@
+package fi.csc.avaa.smear.resource;
+
+import fi.csc.avaa.smear.constants.Endpoints;
+
+import javax.ws.rs.Path;
+
+@Path(Endpoints.TABLE_METADATA)
+public class TableMetadataResource {
+}
