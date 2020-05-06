76523ff3cc983799f7fbe27a5a449afea4683641
Mikko Kautto <mikko.kautto@codento.com>
2020-05-06 13:58:18 +0300

    Some more documentation for time series endpoint

--- src/main/java/fi/csc/avaa/smear/parameter/TimeSeriesSearch.java
+++ src/main/java/fi/csc/avaa/smear/parameter/TimeSeriesSearch.java
@@ -22,27 +22,33 @@ public class TimeSeriesSearch {
     @Parameter(description = "Name of the database table where variable data is stored in the smear database. " +
             "If you want to select from multiple tables, please use multiple tablevariable parameters. " +
             "Table names can be found from the tablemetadata endpoint by variables tableIDs. " +
-            "TableID for every variable can be found from variable's metadata record via the variablemetadata endpoint. ")
+            "TableID for every variable can be found from variable's metadata record via the variablemetadata endpoint. ",
+            example = "HYY_META")
     @QueryParam("table")
     public String table;
 
-
+    @Parameter(description = "Name of a variable in the SMEAR database. Multiple parameters can be used. " +
+            "At least one is required if the table parameter is not empty.",
+            example = "Pamb0")
     @QueryParam("variable")
     public List<String> variables;
 
-    @Parameter(description = "Name of table and variable separated by a period (e.g. HYY_META.Pamb0). " +
-            "Multiple tablevariable parameters can be used.")
+    @Parameter(description = "Name of table and variable separated by a period " +
+            "Multiple tablevariable parameters can be used.",
+            example = "HYY_META.Pamb0")
     @QueryParam("tablevariable")
     public List<@Pattern(regexp = "[a-zA-Z0-9_]+\\.[a-zA-Z0-9_]+") String> tableVariables;
 
-    @Parameter(description = "Time series start time in ISO 8601 format (YYYY-MM-DDThh:mm:ss.mmm)",
+    @Parameter(description = "Time series start time in ISO 8601 format (YYYY-MM-DDThh:mm:ss.mmm).",
+            example = "2016-02-11T00:00:00.989",
             required = true)
     @QueryParam("from")
     @NotNull
     @ValidIsoDate
     public String from;
 
-    @Parameter(description = "Time series end time in ISO 8601 format (YYYY-MM-DDThh:mm:ss.mmm)",
+    @Parameter(description = "Time series end time in ISO 8601 format (YYYY-MM-DDThh:mm:ss.mmm).",
+            example = "2016-02-12T09:06:07.989",
             required = true)
     @QueryParam("to")
     @NotNull
@@ -50,6 +56,7 @@ public class TimeSeriesSearch {
     public String to;
 
     @Parameter(description = "Should the time series data be quality checked or not. Valid values: ANY, CHECKED.",
+            example = "ANY",
             required = true)
     @QueryParam("quality")
     @NotNull
@@ -57,11 +64,13 @@ public class TimeSeriesSearch {
     public String quality;
 
     @Parameter(description = "Type of the sample time aggregation. " +
-            "Valid values: NONE (default), ARITHMETIC, GEOMETRIC, SUM, MEDIAN, MIN, MAX, CIRCULAR.")
+            "Valid values: NONE (default), ARITHMETIC, GEOMETRIC, SUM, MEDIAN, MIN, MAX, CIRCULAR.",
+            example = "NONE")
     @QueryParam("aggregation_type")
     public String aggregationTypeStr;
 
-    @Parameter(description = "Sample time aggregation interval. Valid values: 30MIN (default), 60MIN.")
+    @Parameter(description = "Sample time aggregation interval. Valid values: 30MIN (default), 60MIN.",
+            example = "30MIN")
     @QueryParam("aggregation_interval")
     public String aggregationIntervalStr;
 
